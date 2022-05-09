28080479bb10e39d0c8ee017914d9bf8a968423e
Benoit Pingris <benoit.pingris@epitech.eu>
2022-05-09 20:35:35 +0200

    Initial commit

---
+++ src/components/tests/CounterTitle.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import CounterTitle from '../CounterTitle.vue'
+
+describe('CounterTitle', () => {
+  test('it should work', () => {
+    const wrapper = mount(CounterTitle, {
+      props: {
+        counter: 42,
+      },
+    })
+
+    expect(wrapper.text()).toContain('The count is at 42!')
+  })
+})
